8bc104551c2e6b894605b2feaf5d6143c559a309
Jarosław Kołdun <jkoldun@gmail.com>
2025-06-29 23:14:25 +0200

    Train on YOLOv8n

--- src/training/train.py
+++ src/training/train.py
@@ -1,4 +1,4 @@
 from ultralytics import YOLO
 
 model = YOLO('yolov8n.pt')
-model.train(data='data.yaml', epochs=50, imgsz=640, batch=16)
+model.train(data='data.yaml', epochs=50, imgsz=640, batch=16, device=[0])
